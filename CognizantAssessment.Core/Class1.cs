1954a40bf620bee2e80e374b5c0c2579cdf6767b
OLAMILEKAN KAREEM <elkaysoft@outlook.com>
2022-02-09 01:35:35 +0100

    Add project files.

---
+++ CognizantAssessment.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CognizantAssessment.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ CognizantAssessment.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CognizantAssessment.Data
+{
+    public class Class1
+    {
+    }
+}
